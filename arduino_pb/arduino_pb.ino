d9eca30cf43c0a4482a38b7acde3ee9ceb5c63b1
Carlos Alberto <oiueei@disroot.org>
2022-11-02 12:13:04 +0100

    Add relay

--- arduino_pb/arduino_pb.ino
+++ arduino_pb/arduino_pb.ino
@@ -1,16 +1,22 @@
 int gasSensorPin = A0;
 int gasValue;
-
+int relayPin = 3;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(gasSensorPin, INPUT);
+  pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
   gasValue=analogRead(gasSensorPin);
   Serial.print("GasValue= ");
   Serial.println(gasValue);
-  delay(500);
+  
+  delay(10000);
+
+  digitalWrite(relayPin, HIGH);
+  delay(300);
+  digitalWrite(relayPin, LOW);
 }
