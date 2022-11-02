0ea3f0c443c9726a9e82aca6b64f145754a1e76e
Carlos Alberto <oiueei@disroot.org>
2022-11-02 15:33:27 +0100

    Add PIR sensor

--- arduino_pb/arduino_pb.ino
+++ arduino_pb/arduino_pb.ino
@@ -1,30 +1,38 @@
 int gasSensorPin = A0;
 int flameSensorPin = A1;
+int pirSensorPin = 2;
 int relayPin = 3;
 
 int flameValue;
 int gasValue;
+int pirValue;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(gasSensorPin, INPUT);
   pinMode(flameSensorPin, INPUT);
+  pinMode(pirSensorPin, INPUT);
   pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
   gasValue=analogRead(gasSensorPin);
-  Serial.print("GasValue= ");
-  Serial.println(gasValue);
+  // Serial.print("GasValue= ");
+  // Serial.println(gasValue);
   
   flameValue=analogRead(flameSensorPin);
-  Serial.print("FlameValue= ");
-  Serial.println(flameValue);
+  // Serial.print("FlameValue= ");
+  // Serial.println(flameValue);
 
   delay(10000);
 
   // digitalWrite(relayPin, HIGH);
   // delay(300);
   // digitalWrite(relayPin, LOW);
+
+  pirValue = digitalRead(pirSensorPin);
+  Serial.print("PirValue= ");
+  Serial.println(pirValue);
+
 }
