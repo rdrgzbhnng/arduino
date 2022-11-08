f4a2f6287393e997ff9d0e38bcf0007a126378e9
Carlos Alberto <oiueei@disroot.org>
2022-11-08 16:19:59 +0100

    Add MQ9 gas sensor, get average RO

---
+++ sensors_vase/sensors_vase.ino
@@ -0,0 +1,27 @@
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  float sensor_volt;
+  float RS_air;
+  float R0;
+  float sensorValue;
+ 
+  for(int x = 0 ; x < 100 ; x++) {
+    sensorValue = sensorValue + analogRead(A0);
+  }
+  sensorValue = sensorValue/100.0;
+
+  sensor_volt = sensorValue/1024*5.0;
+  RS_air = (5.0-sensor_volt)/sensor_volt;
+  R0 = RS_air/9.9;
+
+  Serial.print("sensor_volt = ");
+  Serial.print(sensor_volt);
+  Serial.println("V");
+
+  Serial.print("R0 = ");
+  Serial.println(R0);
+  delay(1000);
+}
