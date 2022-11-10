003ab260647a29ddc82befa3ab4b04743402b3df
Carlos Alberto <oiueei@disroot.org>
2022-11-10 12:06:11 +0100

    Calibrate soil moisture sensors

--- coop_garden/coop_garden.ino
+++ coop_garden/coop_garden.ino
@@ -1,18 +1,63 @@
 int waterSensorPin = 2;
 int dustSensorPin = 4;
 
+int soilMoistureSensor01Pin = A0;
+int soilMoisture01Value = 0;
+int soilMoisture01Percent=0;
+const int AIR_VALUE01 = 20;
+const int WATER_VALUE01 = 620;
+
+int soilMoistureSensor02Pin = A1;
+int soilMoisture02Value = 0;
+int soilMoisture02Percent=0;
+const int AIR_VALUE02 = 10;
+const int WATER_VALUE02 = 630;
+
+int soilMoistureSensor03Pin = A2;
+int soilMoisture03Value = 0;
+int soilMoisture03Percent=0;
+const int AIR_VALUE03 = 10;
+const int WATER_VALUE03 = 720;
+
+int soilMoistureSensor04Pin = A3;
+int soilMoisture04Value = 0;
+int soilMoisture04Percent=0;
+const int AIR_VALUE04 = 690;
+const int WATER_VALUE04 = 470;
+
+int soilMoistureSensor05Pin = A4;
+int soilMoisture05Value = 0;
+int soilMoisture05Percent=0;
+const int AIR_VALUE05 = 730;
+const int WATER_VALUE05 = 420;
+
+int soilMoistureSensor06Pin = A5;
+int soilMoisture06Value = 0;
+int soilMoisture06Percent=0;
+const int AIR_VALUE06 = 500;
+const int WATER_VALUE06 = 270;
+
 unsigned long duration;
 unsigned long startTime;
 
-const long SAMPLE_TIME = 30000;
+const long SAMPLE_TIME = 60000;
 unsigned long lowPulseOccupancy = 0;
 
 float ratio = 0;
 float concentration = 0;
 
+int waterValue;
+String waterStatus;
+
 void setup() {
   Serial.begin(9600);
 
+  pinMode(soilMoistureSensor01Pin, INPUT);
+  pinMode(soilMoistureSensor02Pin, INPUT);
+  pinMode(soilMoistureSensor03Pin, INPUT);
+  pinMode(soilMoistureSensor04Pin, INPUT);
+  pinMode(soilMoistureSensor05Pin, INPUT);
+  pinMode(soilMoistureSensor06Pin, INPUT);
   pinMode(waterSensorPin, INPUT);
   pinMode(dustSensorPin, INPUT);
 
@@ -27,23 +72,152 @@ void loop() {
     ratio = lowPulseOccupancy / (SAMPLE_TIME * 10.0);
     concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
 
-    int waterValue = (digitalRead(waterSensorPin));
-    String waterStatus = "";
+    waterValue = (digitalRead(waterSensorPin));
+    waterStatus = "";
 
     if (waterValue == 1) {
       waterStatus = "Nops";
     } else {
       waterStatus = "Yep!";
     }
+
+    // Serial.print("lowpulseoccupancy = ");
+    // Serial.println(lowPulseOccupancy);
+    // Serial.print("ratio = ");
+    // Serial.println(ratio);
+    // Serial.print("concentration = ");
+    // Serial.println(concentration);
+    // Serial.print("there is water? ");
+    // Serial.println(waterStatus);
     
-    Serial.print("lowpulseoccupancy = "); // remover
-    Serial.println(lowPulseOccupancy); // remover
-    Serial.print("ratio = "); // remover
-    Serial.println(ratio); // remover
-    Serial.print("concentration = "); // remover
-    Serial.println(concentration); // remover
-    Serial.print("there is water? "); // remover
-    Serial.println(waterStatus); // remover
+    soilMoisture01Value = analogRead(soilMoistureSensor01Pin);
+    soilMoisture02Value = analogRead(soilMoistureSensor02Pin);
+    soilMoisture03Value = analogRead(soilMoistureSensor03Pin);
+    soilMoisture04Value = analogRead(soilMoistureSensor04Pin);
+    soilMoisture05Value = analogRead(soilMoistureSensor05Pin);
+    soilMoisture06Value = analogRead(soilMoistureSensor06Pin);
+
+    soilMoisture01Percent = map(soilMoisture01Value, AIR_VALUE01, WATER_VALUE01, 0, 100);
+    soilMoisture02Percent = map(soilMoisture02Value, AIR_VALUE02, WATER_VALUE02, 0, 100);
+    soilMoisture03Percent = map(soilMoisture03Value, AIR_VALUE03, WATER_VALUE03, 0, 100);
+    soilMoisture04Percent = map(soilMoisture04Value, AIR_VALUE04, WATER_VALUE04, 0, 100);
+    soilMoisture05Percent = map(soilMoisture05Value, AIR_VALUE05, WATER_VALUE05, 0, 100);
+    soilMoisture06Percent = map(soilMoisture06Value, AIR_VALUE06, WATER_VALUE06, 0, 100);
+
+    Serial.println("---------------------------------");
+    Serial.println("---------------------------------");
+
+    Serial.print("AIR_VALUE01 = ");
+    Serial.println(AIR_VALUE01);
+    Serial.print("WATER_VALUE01 = ");
+    Serial.println(WATER_VALUE01);
+    Serial.print("soilMoisture01Value = ");
+    Serial.println(soilMoisture01Value);
+
+    Serial.print("soilMoisture01Percent = ");
+    if(soilMoisture01Percent >= 100) {
+      Serial.println("100 %");
+    } else if(soilMoisture01Percent <=0) {
+      Serial.println("0 %");
+    } else if(soilMoisture01Percent >0 && soilMoisture01Percent < 100) {
+      Serial.print(soilMoisture01Percent);
+      Serial.println("%");
+    }
+
+    Serial.println("---------------------------------");
+
+    Serial.print("AIR_VALUE02 = ");
+    Serial.println(AIR_VALUE02);
+    Serial.print("WATER_VALUE02 = ");
+    Serial.println(WATER_VALUE02);
+    Serial.print("soilMoisture02Value = ");
+    Serial.println(soilMoisture02Value);
+    
+    Serial.print("soilMoisture02Percent = ");
+    if(soilMoisture02Percent >= 100) {
+      Serial.println("100 %");
+    } else if(soilMoisture02Percent <=0) {
+      Serial.println("0 %");
+    } else if(soilMoisture02Percent >0 && soilMoisture02Percent < 100) {
+      Serial.print(soilMoisture02Percent);
+      Serial.println("%");
+    }
+
+    Serial.println("---------------------------------");
+
+    Serial.print("AIR_VALUE03 = ");
+    Serial.println(AIR_VALUE03);
+    Serial.print("WATER_VALUE03 = ");
+    Serial.println(WATER_VALUE03);
+    Serial.print("soilMoisture03Value = ");
+    Serial.println(soilMoisture03Value);
+
+    Serial.print("soilMoisture03Percent = ");
+    if(soilMoisture03Percent >= 100) {
+      Serial.println("100 %");
+    } else if(soilMoisture03Percent <=0) {
+      Serial.println("0 %");
+    } else if(soilMoisture03Percent >0 && soilMoisture03Percent < 100) {
+      Serial.print(soilMoisture03Percent);
+      Serial.println("%");
+    }
+
+    Serial.println("---------------------------------");
+
+    Serial.print("AIR_VALUE04 = ");
+    Serial.println(AIR_VALUE04);
+    Serial.print("WATER_VALUE04 = ");
+    Serial.println(WATER_VALUE04);
+    Serial.print("soilMoisture04Value = ");
+    Serial.println(soilMoisture04Value);
+
+    Serial.print("soilMoisture04Percent = ");
+    if(soilMoisture04Percent >= 100) {
+      Serial.println("100 %");
+    } else if(soilMoisture04Percent <=0) {
+      Serial.println("0 %");
+    } else if(soilMoisture04Percent >0 && soilMoisture04Percent < 100) {
+      Serial.print(soilMoisture04Percent);
+      Serial.println("%");
+    }
+
+    Serial.println("---------------------------------");
+
+    Serial.print("AIR_VALUE05 = ");
+    Serial.println(AIR_VALUE05);
+    Serial.print("WATER_VALUE05 = ");
+    Serial.println(WATER_VALUE05);
+    Serial.print("soilMoisture05Value = ");
+    Serial.println(soilMoisture05Value);
+
+    Serial.print("soilMoisture05Percent = ");
+    if(soilMoisture05Percent >= 100) {
+      Serial.println("100 %");
+    } else if(soilMoisture05Percent <=0) {
+      Serial.println("0 %");
+    } else if(soilMoisture05Percent >0 && soilMoisture05Percent < 100) {
+      Serial.print(soilMoisture05Percent);
+      Serial.println("%");
+    }
+
+    Serial.println("---------------------------------");
+
+    Serial.print("AIR_VALUE06 = ");
+    Serial.println(AIR_VALUE06);
+    Serial.print("WATER_VALUE06 = ");
+    Serial.println(WATER_VALUE06);
+    Serial.print("soilMoisture06Value = ");
+    Serial.println(soilMoisture06Value);
+
+    Serial.print("soilMoisture06Percent = ");
+    if(soilMoisture06Percent >= 100) {
+      Serial.println("100 %");
+    } else if(soilMoisture06Percent <=0) {
+      Serial.println("0 %");
+    } else if(soilMoisture06Percent >0 && soilMoisture06Percent < 100) {
+      Serial.print(soilMoisture06Percent);
+      Serial.println("%");
+    }
     
     lowPulseOccupancy = 0;
     
