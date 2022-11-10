417f2973650cd048257870dd4103b762e59bfa6e
Carlos Alberto <oiueei@disroot.org>
2022-11-10 08:10:35 +0100

    Ready to tryout

--- kitchen_alarm/kitchen_alarm.ino
+++ kitchen_alarm/kitchen_alarm.ino
@@ -11,10 +11,10 @@ int pirValue;
 unsigned long duration;
 unsigned long startTime;
 
-const long SAMPLE_TIME = 30000;  // subir a 60000
-const int GAS_WARNING = 200;  // subir a 300
-const int FLAME_WARNING = 250; // subir a 400
-const long HARD_ALARM = 1000; // subir a 300000
+const long SAMPLE_TIME = 60000;
+const int GAS_WARNING = 300;
+const int FLAME_WARNING = 400;
+const long HARD_ALARM = 300000;
 const int PIR_STATUS = 0;
 
 unsigned long issue_01 = 0;
@@ -41,13 +41,6 @@ void loop() {
     flameValue = analogRead(flameSensorPin);
     pirValue = digitalRead(pirSensorPin);
 
-    Serial.print("gasValue = "); // remover
-    Serial.println(gasValue); // remover
-    Serial.print("flameValue = "); // remover
-    Serial.println(flameValue); // remover
-    Serial.print("pirValue = "); // remover
-    Serial.println(pirValue); // remover
-
     if ((gasValue > GAS_WARNING) || (flameValue > FLAME_WARNING)) {
       soft_alarm();
       hard_alarm();
@@ -57,8 +50,8 @@ void loop() {
 }
 
 void soft_alarm() {
-  for (int i = 1; i <= 3; i += 1) { // subir a <= 10
-    tone(buzzPin, 100, 30); // subir a 300
+  for (int i = 1; i <= 10; i += 1) {
+    tone(buzzPin, 100, 300);
     delay(1000);
     noTone(buzzPin);
     delay(300);
@@ -71,23 +64,11 @@ void hard_alarm() {
   issue_02 = issue_03;
   issue_03 = millis();
 
-  Serial.print("issue_01 = "); // remover
-  Serial.println(issue_01); // remover
-  Serial.print("issue_02 = "); // remover
-  Serial.println(issue_02); // remover
-  Serial.print("issue_03 = "); // remover
-  Serial.println(issue_03); // remover
-
   if (issue_01 > 0 ) {
     if ((issue_03 - issue_02) < (SAMPLE_TIME * 2)) {
       if ((issue_02 - issue_01) < (SAMPLE_TIME * 2)) {
         pirValue = digitalRead(pirSensorPin);
         if (pirValue == 0) {
-
-          Serial.print("pirValue = "); // remover
-          Serial.println(pirValue); // remover
-          Serial.print("PIR_STATUS = "); // remover
-          Serial.println(PIR_STATUS); // remover
 
           digitalWrite(relayPin, HIGH);
           delay(HARD_ALARM);
