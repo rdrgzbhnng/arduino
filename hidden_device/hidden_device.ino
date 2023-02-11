d9f19564eb6bf47f78551e5df931ddb2a431deee
rdrgzbhnng <oiueei@disroot.org>
2023-02-11 23:34:37 +0100

    Manage flow with millis instead of delays

--- hidden_device/hidden_device.ino
+++ hidden_device/hidden_device.ino
@@ -1,47 +1,57 @@
-const int FSR_PIN        = A0;
-const int ALARM_PIN      =  9;
-const int ALARM_CONTROL  = 11;
+const int FSR_PIN       =      A0;
+const int RESET_PIN     =       7;
+const int ALARM_PIN     =       9;
+const int ALARM_CONTROL =      11;
+const int SAMPLE_TIME   =   10000;
 
 int fsrSensor;
-int controlDelay     =   3000;
-int alarmDelay      =     600;
-int alarmRunning    =  3000; // 180000
-int beepDelay       =     100;
+int alarmDelay          =     500;
+int alarmRunning        =  180000;
+int beepDelay           =     100;
+
+unsigned long duration;
+unsigned long controlTime;
+
 
 void setup() {
+  digitalWrite(RESET_PIN, HIGH);
+  digitalWrite(ALARM_PIN, HIGH);
+
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ALARM_CONTROL, OUTPUT);
+  pinMode(RESET_PIN, OUTPUT);
   pinMode(ALARM_PIN, OUTPUT);
   pinMode(FSR_PIN, INPUT);
 
-  digitalWrite(ALARM_PIN, HIGH);
+  controlTime = millis();
 }
 
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(alarmDelay);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(alarmDelay);
+  if ((millis() - controlTime) > SAMPLE_TIME) {
+    digitalWrite(LED_BUILTIN, HIGH);
 
-  fsrSensor = analogRead(FSR_PIN);
-  Serial.print("FSR reading: ");
-  Serial.println(fsrSensor);
+    fsrSensor = analogRead(FSR_PIN);
+    Serial.print("FSR reading: ");
+    Serial.println(fsrSensor);
+    Serial.print("Control time: ");
+    Serial.println(controlTime);
 
-  if ((fsrSensor > 720) && (fsrSensor <= 970)) {
-  beep();
-  delay(controlDelay);
+    if (controlTime > 21600000) {
+      reset();
+    }
 
-  } else if ((fsrSensor > 550) && (fsrSensor < 720)) {
-  beep(); beep();
-  delay(controlDelay);
+    if ((fsrSensor > 680) && (fsrSensor <= 920)) {
+      beep();
+    } else if ((fsrSensor > 550) && (fsrSensor <= 680)) {
+      beep(); beep();
+    } else {
+      beep(); beep(); beep();
+      siren();
+    }
 
-  } else {
-  beep(); beep(); beep();
-
-  digitalWrite(ALARM_PIN, LOW);
-  Serial.println("THE ALARM IS GOING OFF!");
-  delay(alarmRunning);
-  digitalWrite(ALARM_PIN, HIGH);
+    controlTime = millis();
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
 
@@ -51,4 +61,17 @@ void beep() {
   delay(beepDelay);
   tone(ALARM_CONTROL, 0);
   delay(alarmDelay);
+}
+
+
+void reset() {
+  digitalWrite(RESET_PIN, LOW);
+}
+
+
+void siren() {
+  digitalWrite(ALARM_PIN, LOW);
+  Serial.println("THE ALARM IS GOING OFF!");
+  delay(alarmRunning);
+  digitalWrite(ALARM_PIN, HIGH);
 }
